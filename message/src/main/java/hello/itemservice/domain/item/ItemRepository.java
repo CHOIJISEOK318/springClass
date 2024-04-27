47bffd3fe4b90e0b4c2d97fadca6d3c254d10b4a
CHOIJISEOK318 <wltjr318@naver.com>
2024-04-28 00:22:06 +0900

    메시지, 국제화

--- message/src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ message/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -11,6 +11,7 @@ import java.util.Map;
 public class ItemRepository {
 
     private static final Map<Long, Item> store = new HashMap<>(); //static
+
     private static long sequence = 0L; //static
 
     public Item save(Item item) {
